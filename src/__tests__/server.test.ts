a62c0a0ae07a9cd74207422d2d956f4cb159d464
ksarpash <ksarpash@btinternet.com>
2025-04-09 14:06:47 +0100

    Added a first failing test for the Card domain model to get the summary for the card.  Also skipped the server integration test for now.

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,11 +1,13 @@
-import request from 'supertest'
-import { app } from '../server'
+import request from "supertest";
+import { app } from "../server";
 
-test('returns matching card title', async () => {
-  const response = await request(app).get('/cards/card001')
+test.skip("returns matching card title", async () => {
+  const response = await request(app).get("/cards/card001");
 
-  expect(response.status).toBe(200)
-  expect(response.body).toEqual(expect.objectContaining({
-    title: 'card 1 title',
-  }))
-})
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(
+    expect.objectContaining({
+      title: "card 1 title",
+    })
+  );
+});
---
+++ src/__tests__/unit/models/card.test.ts
@@ -0,0 +1,58 @@
+interface CardDTO {
+  id: string;
+  title: string;
+  sizes: string[];
+  basePrice: number;
+  pages: {
+    title: string;
+    templateId: string;
+  }[];
+}
+
+interface Card {
+  getSummary: () => {};
+}
+
+const createCard = (card): { getSummary: () => { title: string } } => {
+  const getSummary = () => {
+    return { title: "" };
+  };
+
+  return {
+    getSummary,
+  };
+};
+
+let cardDTO = {
+  id: "card001",
+  title: "card 1 title",
+  sizes: ["sm", "md", "gt"],
+  basePrice: 200,
+  pages: [
+    {
+      title: "Front Cover",
+      templateId: "template001",
+    },
+    {
+      title: "Inside Left",
+      templateId: "template002",
+    },
+    {
+      title: "Inside Right",
+      templateId: "template003",
+    },
+    {
+      title: "Back Cover",
+      templateId: "template004",
+    },
+  ],
+};
+
+describe("Card Domain", () => {
+  const card = createCard(cardDTO);
+
+  test("getSummary returns correct summary details", () => {
+    const summary = card.getSummary();
+    expect(summary.title).toBe("card 1 title");
+  });
+});
