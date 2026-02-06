95f15116a759daaeebb6556c78b02be0227b5b04
Ross Lockhart <ross.lockhart@moonpig.com>
2026-02-06 14:24:58 +0000

    feat: implement cards endpoint with image

--- NOTES.md
+++ NOTES.md
@@ -1,1 +1,14 @@
 Please add any additional notes here…
+
+
+high level slices: 
+run server
+load and inspect json data
+expose two endpoints 
+return appropriate Json responses 
+
+slices of slices: 
+- Save the url as consts here in the server file?  
+- add a function(s) to get the data (could write just one that takes a url as an arg, and then I can call that in more specific functions later?)
+These could then be called in the GET. 
+- save the correct props/target the correct props, to a var. 
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,49 @@ export const app = express()
 
 app.set('json spaces', 2);
 
-app.get('/cards', async () => {
-  // respond with a list of cards
+//List of URL
+const cardsUrl = "https://moonpig.github.io/tech-test-node-backend/cards.json"
+const sizesUrl = "https://moonpig.github.io/tech-test-node-backend/sizes.json"
+const templatesUrl = "https://moonpig.github.io/tech-test-node-backend/templates.json"
+
+//helper to get data 
+async function fetchJson(url: string) {
+  const res = await fetch(url);
+  if (!res.ok) throw new Error(`Failed to fetch ${url}`);
+  return res.json();
+}
+
+// function fir first endpoint
+async function getAllCards() {
+  return fetchJson(cardsUrl);
+}
+async function getTemplates() {
+  return fetchJson(templatesUrl);
+}
+
+
+app.get('/cards', async (req, res) => {
+  try{
+    const cards = await getAllCards();
+    const templates = await getTemplates();
+
+    const result = cards.map((card) => {
+// hit pages array on card to compare templateID
+      const firstPage = card.pages[0];
+// find corresponding template for id cited in page array
+const firstPageTemplate = templates.find(t => t.id === firstPage.templateId);
+
+return {
+        title: card.title,
+        url: `/cards/${card.id}`,
+        imageUrl: firstPageTemplate ? firstPageTemplate.imageUrl : "No image available",
+      };
+    })
+   res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch cards" });
+  }
 })
 
 app.get('/cards/:cardId/:sizeId?', () => {
