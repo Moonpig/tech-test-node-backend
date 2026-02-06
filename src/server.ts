f01327439b5e87890b03abb1ba9c3ed46b8034ce
Ross Lockhart <ross.lockhart@moonpig.com>
2026-02-06 16:55:10 +0000

    chore: remove comments and update notes

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,16 @@ export const app = express()
 
 app.set('json spaces', 2);
 
-//List of URL
 const cardsUrl = "https://moonpig.github.io/tech-test-node-backend/cards.json"
 const sizesUrl = "https://moonpig.github.io/tech-test-node-backend/sizes.json"
 const templatesUrl = "https://moonpig.github.io/tech-test-node-backend/templates.json"
 
-//helper to get data 
 async function fetchJson(url: string) {
   const res = await fetch(url);
   if (!res.ok) throw new Error(`Failed to fetch ${url}`);
   return res.json();
 }
 
-// function fir first endpoint
 async function getAllCards() {
   return fetchJson(cardsUrl);
 }
@@ -34,9 +31,7 @@ app.get('/cards', async (req, res) => {
     const templates = await getTemplates();
 
     const result = cards.map((card) => {
-// hit pages array on card to compare templateID
       const firstPage = card.pages[0];
-// find corresponding template for id cited in page array
 const firstPageTemplate = templates.find(t => t.id === firstPage.templateId);
 
 return {
@@ -54,31 +49,25 @@ return {
 
 app.get('/cards/:cardId/:sizeId?', async (req, res) => {
   try{
-  // save what params user typed in
   const { cardId, sizeId } = req.params;
   const cards = await getAllCards();
   const templates = await getTemplates();
   const sizes = await getSizes();
 
-  // find card matching id
   const card = cards.find(card => card.id === cardId);
   if (!card) {
     return res.status(404).json({ error: "Card not found" });
   }
-  // get size requested or set to be undfined if not provided
   const selectedSize = sizeId
   ? sizes.find(s => s.id === sizeId)
   : undefined;
 
-  // 404 if given size param doesn't match any in data
     if (sizeId && !selectedSize) {
       return res.status(404).json({ error: "Card size not found" });
     }
-    // format price
     const pricePence = selectedSize ? card.basePrice * selectedSize.priceMultiplier : card.basePrice;
     const price = `£${(pricePence / 100).toFixed(2)}`; 
 
-  // get template for card
   const template  = templates.find(t => t.id === card.pages[0].templateId);
 
   res.json({
