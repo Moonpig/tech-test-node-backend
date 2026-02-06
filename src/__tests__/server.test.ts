5db6765f893cae8e3be2e7b53370bbcbf550a7b4
Ross Lockhart <ross.lockhart@moonpig.com>
2026-02-06 14:37:25 +0000

    test: add unit test for returning from cards endpoint

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,6 +1,16 @@
 import request from 'supertest'
 import { app } from '../server'
 
+
+test('returns all cards with correct structure', async () => {
+  const response = await request(app).get('/cards')
+
+  expect(response.status).toBe(200)
+  expect(response.body).toEqual(
+  expect.arrayContaining([
+    expect.objectContaining({ title: 'card 1 title', url: '/cards/card001' })
+  ]))
+})
 test('returns matching card title', async () => {
   const response = await request(app).get('/cards/card001')
 
